a640b3483e277454f81a28c553a294ca5ebea1ce
Thomas Baumann <tbthomas.baumann@gmail.com>
2022-02-15 12:29:28 -0500

    finished all features using 7 CRUD routes, refactoring needed

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   post '/tasks', to: 'tasks#create'
 
   get '/task/:id', to: 'tasks#show', as: :task
+
+  get '/task/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch '/task/:id', to: 'tasks#update'
+
+  delete '/task/:id', to: 'tasks#destroy', as: :task_destroy
 end
